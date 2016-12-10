2ba3d9058b7cf88ef84d986f2f96aecb7e9703b9
Hashj4ck <christian.bargmann@haw-hamburg.de>
2016-12-10 15:40:23 +0100

    Turnierwerkzeug erstellt

---
+++ werkzeuge/subwerkzeuge/turnierwerkzeug/Rundenberechner.java
@@ -0,0 +1,28 @@
+package werkzeuge.subwerkzeuge.turnierwerkzeug;
+
+import java.util.List;
+
+import materialien.Team;
+import materialien.Teilnehmer;
+
+/**
+ * @author Christian Bargmann <christian.bargmann@haw-hamburg.de>
+ * @version 10.12.2016
+ * @see werkzeuge.subwerkzeuge.turnierwerkzeug
+ * @since 10.12.2016 , 15:31:05
+ *
+ */
+public class Rundenberechner {
+
+	/**
+	 * Konstruktor fuer neue Exemplare der Klasse Rundenberechner.
+	 */
+	public Rundenberechner() {
+
+	}
+
+	private List<Team> erstelleTeams(List<Teilnehmer> teilnehmerliste) {
+
+	}
+
+}
